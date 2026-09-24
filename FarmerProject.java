6f7933176d8384e15587e4be444c87fceb27e7a4
Amogh <andraamogh@gmail.com>
2026-09-24 10:37:20 +0530

    Create initial Java project

---
+++ FarmerProject.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class FarmerProject {
+
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("FARMER'S MARKET PRICE AND SALES TRACKER");
+
+    }
+}
